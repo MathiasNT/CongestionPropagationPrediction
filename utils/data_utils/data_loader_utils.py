526bf1a5f6507e036247f93692f1335f367cf926
Mathias Niemann Tygesen <mnity@dtu.dk>
2022-09-20 04:23:06 +0200

    update dataloadermodule to follow transform style

--- utils/data_utils/data_loader_utils.py
+++ utils/data_utils/data_loader_utils.py
@@ -1,9 +1,29 @@
 from importlib.resources import path
-from locale import normalize
 from torch.utils.data import Dataset, DataLoader, random_split
 import torch
 import pytorch_lightning as pl
 import numpy as np
+
+
+
+class Standardize:
+    def __call__(self, train_set, input_full, padded_lane_mask):
+        # Get mean and std based on train set
+        train_input = input_full[train_set.indices]
+        masked_input = train_input[padded_lane_mask[train_set.indices]]
+        input_obs_mean = masked_input[..., :3].mean((0,1))
+        input_obs_std = masked_input[..., :3].std((0,1))
+
+        # Normalize the full data
+        standardized_input_obs = (input_full[..., :3] - input_obs_mean) / input_obs_std
+
+        standardized_input = torch.ones_like(input_full) * -1
+        standardized_input[..., :3] = standardized_input_obs
+        standardized_input[..., 3:] = input_full[..., 3:]
+
+        standardized_input[~padded_lane_mask] = -1
+
+        return standardized_input, {'mean': input_obs_mean, 'std':input_obs_std}
 
 
 class IncidentDataSet(Dataset):
@@ -47,16 +67,27 @@ class IncidentDataModule(pl.LightningDataModule):
         Attributes:
             batch_size (int): Batch size for the dataloaders
             folder_path (str): Path to the folder with the data
-            train_frac (float): Fraction of data to be used for the train set, the rest is split 50/50 to val and test sets
 
-            Datasets for the Dataloader  - see IncidentDataSet for specifics 
+                Datasets for the Dataloader  - see IncidentDataSet for specifics 
+            train_frac (float): Fraction of data to be used for the train set, the rest is split 50/50 to val and test sets
+            input_val/test/train (torch.Tensor): The input datasets for the val, test and train dataloaders
+            target_val/test/train (torch.Tensor): The target datasets for the val, test and train dataloaders
+            incident_info_val/test/train (torch.Tensor): The incident_info datasets for the val, test and train dataloaders
+            network_info_val/test/train (torch.Tensor): The network_infor datasets for the val, test and train dataloaders
+
+            full_pos weight (float): reweighting factor based on full network for the positive class for the BCE loss 
+                                    (# negative obs / # positive obs)
+            incident_edge_pos weight (float): reweighting factor based only on incident edge for the positive class for the 
+                                             BCE loss (# negative obs / # positive obs)
+
     """
 
-    def __init__(self, folder_path, train_frac=0.6,  batch_size=32):
+    def __init__(self, folder_path, transform=Standardize(), train_frac=0.6,  batch_size=32):
         super().__init__()
         self.batch_size = batch_size
         self.folder_path = folder_path
-        self.train_frac = train_frac 
+        self.train_frac = train_frac
+        self.transform = transform
 
     def prepare_data(self):
         # TODO implement check of the folder here -> have code in notebbok on data preprocess
@@ -79,11 +110,11 @@ class IncidentDataModule(pl.LightningDataModule):
         test_val_len = int(np.round((len(input_full) - train_len) * 0.5))
         train_set, val_set, test_set = random_split(range(len(input_full)), [train_len, test_val_len,test_val_len])
 
-        normalized_input, self.input_obs_mean, self.input_obs_std = self.normalize_input(train_set, input_full, padded_lane_mask)
+        transformed_input, self.transform_params = self.transform(train_set, input_full, padded_lane_mask)
 
-        self.input_train = normalized_input[train_set.indices]
-        self.input_val = normalized_input[val_set.indices]
-        self.input_test = normalized_input[test_set.indices]
+        self.input_train = transformed_input[train_set.indices]
+        self.input_val = transformed_input[val_set.indices]
+        self.input_test = transformed_input[test_set.indices]
 
         self.target_train = target_full[train_set.indices]
         self.target_val = target_full[val_set.indices]
@@ -96,6 +127,11 @@ class IncidentDataModule(pl.LightningDataModule):
         self.network_info_train = network_info_full[train_set.indices]
         self.network_info_val = network_info_full[val_set.indices]
         self.network_info_test = network_info_full[test_set.indices]
+
+        # TODO This part should be moved to preprocessing
+        self.full_pos_weight = (self.target_train[...,0] == 0).sum() / self.target_train[...,0].sum()
+        incident_edge_target = self.target_train[(self.network_info_train[...,0] == 0)]
+        self.incident_edge_pos_weight =  (incident_edge_target[...,0] == 0).sum() / (incident_edge_target[...,0]).sum()
 
     def train_dataloader(self):
         train_split = IncidentDataSet(self.input_train,
@@ -118,21 +154,3 @@ class IncidentDataModule(pl.LightningDataModule):
                                       self.network_info_test)
         return DataLoader(test_split, batch_size=self.batch_size, num_workers=8)
 
-    @staticmethod
-    def normalize_input(train_set, input_full, padded_lane_mask):
-        # Get mean and std based on train set
-        train_input = input_full[train_set.indices]
-        masked_input = train_input[padded_lane_mask[train_set.indices]]
-        input_obs_mean = masked_input[..., :3].mean((0,1))
-        input_obs_std = masked_input[..., :3].std((0,1))
-
-        # Normalize the full data
-        normalized_input_obs = (input_full[..., :3] - input_obs_mean) / input_obs_std
-
-        normalized_input = torch.ones_like(input_full) * -1
-        normalized_input[..., :3] = normalized_input_obs
-        normalized_input[..., 3:] = input_full[..., 3:]
-
-        normalized_input[~padded_lane_mask] = -1
-
-        return normalized_input, input_obs_mean, input_obs_std
