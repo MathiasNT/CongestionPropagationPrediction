e189f18a980d60d7f0456eb4c4d6ff7a72193bd8
Mathias Niemann Tygesen <mnity@dtu.dk>
2022-10-05 18:41:09 +0200

    refactor part 2 clean up

--- convert_results_to_csv.py
+++
@@ -1,9 +0,0 @@
-from utils.results_utils import xml2csv_file, xml2csv_path
-import argparse
-import os
-
-if __name__ == "__main__":
-    arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument("--path", type=str, help='Path to folder or xml file. If folder all files in folder will be converted')
-    args = arg_parser.parse_args()
-    xml2csv_path(args.path)
